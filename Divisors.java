ad18dea8320114b8da3603ba84b7700ce4fd56ef
Zara-Jacobson <zara.jacobson@post.runi.ac.il>
2024-01-01 10:46:30 +0200

    Divisors completed

--- Divisors.java
+++ Divisors.java
@@ -5,7 +5,6 @@ public class Divisors {
 	public static void main (String[] args) {
 		int number = Integer.parseInt(args[0]);
 
-        System.out.println("Divisors of " + number + ":");
         for (int i = 1; i <= number; i++) {
             if (number % i == 0) {
                 System.out.println(i);
