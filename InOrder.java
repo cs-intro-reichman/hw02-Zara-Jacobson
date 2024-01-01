3d534d54cd5993a77061a10a61562f9dd8479fc0
Zara-Jacobson <zara.jacobson@post.runi.ac.il>
2024-01-01 11:28:02 +0200

    InOrder complete

--- InOrder.java
+++ InOrder.java
@@ -5,5 +5,16 @@
 public class InOrder {
 	public static void main (String[] args) {
 		//// Write your code here
+
+		int num1 = (int)(Math.random()*10);
+		System.out.println(num1);
+
+		int num2 = (int)(Math.random()*10);
+
+		while (num2 >= num1) {
+			System.out.print(" "+num2);
+			num1 = num2;
+			num2 = (int)(Math.random()*10);
+		}
 	}
 }
