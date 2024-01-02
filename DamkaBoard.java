cedbc4aa60639da6b2a9ba2554f1acfb189fb97a
Zara-Jacobson <zara.jacobson@post.runi.ac.il>
2024-01-02 19:57:40 +0200

    DamkaBoard complete

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,5 +4,22 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
-	}
+      
+        int n = Integer.parseInt(args[0]);
+
+        printDamkaBoard(n);
+    }
+
+    private static void printDamkaBoard(int n) {
+        
+        for (int i = 0; i < n; i++) {
+            
+            for (int j = 0; j < n; j++) {
+                
+                System.out.print((i + j) % 2 == 0 ? "* " : "  ");
+            }
+            
+            System.out.println();
+        }
+    }
 }
