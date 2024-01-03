5ad753f89a5ef9b8d5caa95efe510e78e5f20344
Zara-Jacobson <zara.jacobson@post.runi.ac.il>
2024-01-03 16:42:14 +0200

    Damka comlete

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,10 +5,10 @@ public class DamkaBoard {
     public static void main(String[] args) {
        
         int n = Integer.parseInt(args[0]);
-        String outpuString = " ";
+        String outpuString = "";
 
         for (int i = 1; i <= n; i++) {
-          outpuString = " ";
+          outpuString = "";
           for (int j = 1; j <= n; j++) {
           
           if (i % 2 != 0) {
