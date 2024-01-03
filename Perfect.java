75f235a54fd76f8b3376fac221770ba2dbd0349f
Zara-Jacobson <zara.jacobson@post.runi.ac.il>
2024-01-03 16:39:43 +0200

    perfect complete

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,27 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
-	}
+		
+     int x = Integer.parseInt(args[0]);
+     int y = 0;
+     String Print = "";
+     for (int i = 1; i <= (x-1); i++) {
+     if ((x % i) == 0) {
+     	y = y+i;
+     	if (i != 1) {
+     		Print = Print + " + " + i;
+     	} else {
+     		Print = "" + i;
+
+     	}
+     }
+    }
+    Boolean n = true;
+    n = (y==x) ? true : false;
+    if (n==true) {
+    	System.out.println(x + " is a perfect number since " + x + " = " + Print);
+    } else {
+    	System.out.println( x + " is not a perfect number");
+    }
+}
 }
