f61f4d272dcec3332c39c6b11446436bb408d229
Zara-Jacobson <zara.jacobson@post.runi.ac.il>
2024-01-03 16:48:33 +0200

    one of each complete

--- OneOfEach.java
+++ OneOfEach.java
@@ -6,5 +6,28 @@
 public class OneOfEach {
 	public static void main (String[] args) {
 		//// Put your code here
-	}
+        boolean hasBoy = false;
+        boolean hasGirl = false;
+        int childrenCount = 0;
+
+        while (!(hasBoy && hasGirl)) {
+            // Randomly decide the gender of the child (0 for boy, 1 for girl)
+            int gender = (int) (Math.random() * 2);
+
+            // Update the boolean variables based on the gender
+            if (gender == 0) {
+                System.out.print("b ");
+                hasBoy = true;
+            } else {
+                System.out.print("g ");
+                hasGirl = true;
+            }
+
+            childrenCount++;
+        }
+
+        System.out.println("\nYou made it... and you now have " + childrenCount + " children.");
+    }
 }
+
+
